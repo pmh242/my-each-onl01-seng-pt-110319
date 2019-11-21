65a2db958be02ecf8b4b577ea8f90f3cc6d5a360
Pedro Huertas <pmh242@gmail.com>
2019-11-21 06:37:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,6 @@
 def my_each(words) # put argument(s) here
   words = ['hi', 'hello', 'bye', 'goodbye']
-    counter = 0
-
-    my_each(words) do |item|
-      expect(item).to equal(words[counter])
-      counter+=1
-    end
+  while words
+    
+  end
 end
